06e9f65079ee147d2a7b0d72dd8c2219bfcb543b
Kris Jordan <krisjordan@gmail.com>
2018-10-29 10:32:00 -0400

    Add pizza example

---
+++ src/21-problem-decomposition/00-pizza-app.ts
@@ -0,0 +1,26 @@
+import { print } from "introcs";
+
+class Pizza {
+    size: string = "small";
+    toppings: number = 0;
+}
+
+export let main = async () => {
+
+    // Given a pizza order:
+    // - large, 3-toppings
+    // - small, 2-toppings
+    
+    // Print a description and price per pizza for any size order.
+    // Print the total price of any size order.
+    
+};
+
+let price = (pizza: Pizza): number => {
+    // The price of a Pizza is
+    // $7 for small, $11 for large
+    // $0.75 per topping
+    return 0;
+};
+
+main();
